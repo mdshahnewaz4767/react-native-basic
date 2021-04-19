5fb320f01b387ddd54099d4bd85370dc04c85c26
shahnewaz601 <shahnewaz601@gmail.com>
2021-04-20 00:20:53 +0600

    handle state change

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
   return (
     <View style={styles.container}>
       <Text>Hi! I am Muhammad Shahnewaz</Text>
+      <Text style={{fontSize: '40px'}}>{name}</Text>
       <Image
         source={{
           uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
         }}
         style={{ width: 200, height: 200 }}
+      />
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        onChangeText={text => setName(text)}
+        defaultValue=""
       />
       <StatusBar style="auto" />
     </View>
